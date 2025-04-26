c74d9508f4c6a18d874d0d4e5c7a71b1e2c4449d
SamraAzizi <samarraazizi@gmail.com>
2025-04-26 12:19:05 +0500

    discord bot

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ token = os.getenv("DISCORD_TOKEN")
 
 handler= logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 intents = discord.Intents.default()
-intents.message_content = True()
+intents.message_content = True
 intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
@@ -18,5 +18,10 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():
     print("We are raedy to go in, {bot.user.name}")
+
+
+@bot.event
+async def on_member_join(member):
+    await member.send(f"Welcome to the server {member.name}")
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
